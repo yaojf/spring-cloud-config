57a75c0e5746edd494fd0a2ef64f1bd61a7cda42
yaojiafeng <yaojiafeng@cai-inc.com>
2017-09-05 09:47:43 +0800

    update

--- spring-cloud-config-client/src/main/java/com/yaojiafeng/spring/cloud/config/Application.java
+++ spring-cloud-config-client/src/main/java/com/yaojiafeng/spring/cloud/config/Application.java
@@ -25,9 +25,5 @@ public class Application {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
-
-        Object properties = applicationContext.getBean("myAutoConfiguration");
-
-        System.out.println(properties);
     }
 }
